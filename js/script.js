3537f4902640c4ef87e48bcee1c68c68f8805a8b
Shashank Shekhar Barik <35847062+SSBsuiit@users.noreply.github.com>
2019-04-28 14:55:09 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,16 @@
+$(document).ready(function () {
+
+    $('.first-button').on('click', function () {
+
+        $('.animated-icon1').toggleClass('open');
+    });
+    $('.second-button').on('click', function () {
+        $('.animated-icon2').toggleClass('open');
+    });
+    $('.third-button').on('click', function () {
+        $('.animated-icon3').toggleClass('open');
+    });
+});
+  
+new WOW().init();
+  
